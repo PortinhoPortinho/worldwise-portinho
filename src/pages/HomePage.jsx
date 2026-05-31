7f2ee42ff5bb9b37b8d7431b10a431966765070f
PortinhoPortinho <m.porto2508@gmail.com>
2026-05-31 20:25:17 -0300

    207 - add PageNav and using link/navlink

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+import PageNav from "../components/PageNav";
 function HomePage() {
   return (
     <div>
+      <PageNav />
       <h1>WorldWise</h1>
+      <Link to="/pricing">Pricing</Link>
     </div>
   );
 }
--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,6 +1,10 @@
+import PageNav from "../components/PageNav";
+
 function PageNotFound() {
   return (
     <div>
+      <PageNav />
+
       <h1>Page Not Found</h1>
     </div>
   );
