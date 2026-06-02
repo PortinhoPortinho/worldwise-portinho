3cbca1a7a12c756bf819a72f03fed76cb0cf2346
PortinhoPortinho <m.porto2508@gmail.com>
2026-06-02 02:36:35 -0300

    231 and 232 display city markers on the map and interacting with the map

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -9,6 +9,15 @@ const formatDate = (date) =>
     year: "numeric",
   }).format(new Date(date));
 
+const flagemojiToPNG = (flag) => {
+  var countryCode = Array.from(flag, (codeUnit) => codeUnit.codePointAt())
+    .map((char) => String.fromCharCode(char - 127397).toLowerCase())
+    .join("");
+  return (
+    <img src={`https://flagcdn.com/24x18/${countryCode}.png`} alt="flag" />
+  );
+};
+
 function CityItem({ city }) {
   const { currentCity } = useCities();
   const { cityName, emoji, date, id, position } = city;
@@ -18,7 +27,7 @@ function CityItem({ city }) {
         className={`${styles.cityItem} ${id === currentCity.id ? styles["cityItem--active"] : ""}`}
         to={`${id}?lat=${position.lat}&lng=${position.lng}`}
       >
-        <span className={styles.emoji}>{emoji}</span>
+        <span>{emoji ? flagemojiToPNG(emoji) : ""}</span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>{formatDate(date)}</time>
         <button className={styles.deleteBtn}>&times;</button>
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,16 +2,31 @@ import styles from "./map.module.css";
 import { useSearchParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useCities } from "../contexts/CitiesContext";
+import { useMap } from "react-leaflet";
+import { useMapEvents } from "react-leaflet";
+
 function Map() {
-  const navigate = useNavigate();
+  const { cities } = useCities();
   const [mapPosition, setMapPosition] = useState([40, 0]);
   const [searchParams, setSearchParams] = useSearchParams();
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
+  const mapLat = searchParams.get("lat");
+  const mapLng = searchParams.get("lng");
+
+  useEffect(
+    function () {
+      if (mapLat && mapLng) {
+        setMapPosition([mapLat, mapLng]);
+      }
+    },
+    [mapLat, mapLng],
+  );
+
   return (
-    <div className={styles.mapContainer} onClick={() => navigate("form")}>
+    <div className={styles.mapContainer}>
       <MapContainer
+        // center={[mapLat, mapLng]}
         center={mapPosition}
         zoom={13}
         scrollWheelZoom={true}
@@ -21,14 +36,38 @@ function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={mapPosition}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {cities.map((city) => {
+          return (
+            <Marker
+              key={city.id}
+              position={[city.position.lat, city.position.lng]}
+            >
+              <Popup>
+                <span className={styles.emoji}>{city.emoji}</span>
+                <h3 className={styles.name}>{city.cityName}</h3>
+              </Popup>
+            </Marker>
+          );
+        })}
+        <ChangeCenter position={mapPosition} />
+        <DetectClick />
       </MapContainer>
     </div>
   );
+}
+
+function DetectClick() {
+  const navigate = useNavigate();
+
+  useMapEvents({
+    click: (e) => navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`),
+  });
+}
+
+function ChangeCenter({ position }) {
+  const map = useMap();
+  map.setView(position);
+  return null;
 }
 
 export default Map;
