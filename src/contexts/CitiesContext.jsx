f302cda6f901c8a62df5922af0252aea4b76fdc8
PortinhoPortinho <m.porto2508@gmail.com>
2026-06-01 23:44:24 -0300

    227 acreating citiescontext

---
+++ src/contexts/CitiesContext.jsx
@@ -0,0 +1,47 @@
+import { createContext, useState, useEffect, useContext } from "react";
+
+const BASE_URL = "http://localhost:9000";
+
+const CitiesContext = createContext();
+
+function CitiesProvider({ children }) {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [country, setCountry] = useState([]);
+  const [notes, setNotes] = useState("");
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("there was an error fetching data...");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchCities();
+  }, []);
+
+  return (
+    <CitiesContext.Provider
+      value={{
+        cities,
+        isLoading,
+      }}
+    >
+      {children}
+    </CitiesContext.Provider>
+  );
+}
+function useCities() {
+  const context = useContext(CitiesContext);
+  if (context === undefined) {
+    throw new Error("useCities was used outside the CitiesProvider");
+  }
+  return context;
+}
+export { CitiesContext, CitiesProvider, useCities };
