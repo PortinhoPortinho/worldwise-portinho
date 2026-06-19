ac38a4b1f7a8b13a5bf7bc5fa02e32910ddfcb75
PortinhoPortinho <m.porto2508@gmail.com>
2026-06-18 21:55:45 -0300

    Update src/components/User.jsx
    Co-authored-by: Guilherme Pereira dos Santos <peguera_eu@live.com>

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -15,7 +15,7 @@ function User() {
     <div className={styles.user}>
       <img src={user.avatar} alt={user.name} />
       <span>Welcome, {user.name}</span>
-      <button onClick={handleClick}>Logout</button>
+      <button onClick={handleLogoutClick}>Logout</button>
     </div>
   );
 }
